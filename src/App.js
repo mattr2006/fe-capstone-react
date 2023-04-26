d22f42db4861ac1f4e018a30b21e57ea3356510b
Matt Reynolds <mattreynolds@dp-c02qd5cvg8wn.home>
2023-04-25 20:46:50 -0600

    Added Home Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import "./components/Navbar.css";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Gallery from "./pages/Gallery";
 import ShowPage from "./pages/Show-Page";
@@ -10,9 +11,11 @@ import ShowPage from "./pages/Show-Page";
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <div className="links-wrapper">
-        <nav>
+        <nav className="navbar">
+          <div classname="link-wrapper">
+            <a href="/home">Home</a>
+          </div>
           <div classname="link-wrapper">
             <a href="/about">About</a>
           </div>
@@ -25,6 +28,11 @@ function App() {
         </nav>
 
         <BrowserRouter>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+          </Switch>
           <Switch>
             <Route path="/about">
               <About />
